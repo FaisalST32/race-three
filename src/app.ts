1a29eae2643e156447bd8c678a185fa6514534bc
Faisal Rashid <faisalrashid32@outlook.com>
2021-09-06 21:00:11 +0530

    A CAR THAT MOVES!!

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import * as THREE from 'three';
+import { TWEEN } from 'three/examples/jsm/libs/tween.module.min';
 import { attachWASDControls, car } from './geometries/car';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { createCones } from './geometries/cones';
 import { createHorizontalPlane } from './geometries/horizontal-plane';
-import Stats from 'three/examples/jsm/libs/stats.module'
+import Stats from 'three/examples/jsm/libs/stats.module';
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xcccccc);
@@ -12,10 +13,15 @@ const fog = new THREE.Fog(0xcccccc, 1, 200);
 scene.fog = fog;
 
 const axes = new THREE.AxesHelper(10);
-
-// scene.add(car);
 scene.add(axes);
+
+// add the car
+car.position.y = 0.5;
+car.castShadow = true;
+scene.add(car);
 // attachWASDControls(car, render);
+
+// add the camera
 const camera = new THREE.PerspectiveCamera(
 	50,
 	window.innerWidth / window.innerHeight,
@@ -23,19 +29,21 @@ const camera = new THREE.PerspectiveCamera(
 	2000
 );
 
-camera.position.z = 60;
-camera.position.y = 20;
+camera.position.z = 15;
+camera.position.y = 10;
 
+// add the cones
 const cones = createCones({
-	planeLength: 100,
-	planeWidth: 80,
+	planeLength: 30,
+	planeWidth: 24,
 	count: 100,
-	maxConeHeight: 10,
-	maxConeRadius: 4,
+	maxConeHeight: 1,
+	maxConeRadius: 0.4,
 });
 
 scene.add(...cones);
 
+// add directional light with shadow
 const light = new THREE.DirectionalLight();
 light.castShadow = true;
 light.shadow.mapSize.width = 2048;
@@ -49,6 +57,73 @@ light.shadow.camera.bottom = -100;
 light.position.z = 60;
 light.position.y = 20;
 scene.add(light);
+
+// key listeners
+let keysHeld: any = {};
+
+document.addEventListener(
+	'keydown',
+	(e: KeyboardEvent) => (keysHeld[e.key.toUpperCase()] = true)
+);
+document.addEventListener(
+	'keyup',
+	(e: KeyboardEvent) => delete keysHeld[e.key.toUpperCase()]
+);
+
+let keyListeners: Function[] = [];
+
+keyListeners.push((keys: any) => {
+	if (!keys.D && !keys.A) {
+		return;
+	}
+	const speed = 0.4;
+	let axis: 'z' | 'x' = 'x';
+	let direction: -1 | 1 = 1;
+	const originalPosition = car.position;
+	switch (true) {
+		case keys.A:
+			axis = 'x';
+			direction = -1;
+			car.rotateY((-direction * Math.PI) / 30);
+			// mesh.position.x -= speed;
+			break;
+		case keys.D:
+			axis = 'x';
+			direction = 1;
+			car.rotateY((-direction * Math.PI) / 30);
+			// mesh.position.x += speed;
+			break;
+
+		default:
+			return;
+	}
+});
+
+keyListeners.push((keys: any) => {
+	if (!keys.W && !keys.S) {
+		return;
+	}
+	const speed = 0.4;
+	let axis: 'z' | 'x' = 'x';
+	let direction: -1 | 1 = 1;
+	const originalPosition = car.position;
+	switch (true) {
+		case keys.W:
+			axis = 'z';
+			direction = -1;
+			car.translateZ(direction * speed);
+			// mesh.position.z -= speed;w
+			break;
+		case keys.S:
+			axis = 'z';
+			direction = 1;
+			car.translateZ(direction * speed);
+			// mesh.position.z += speed;
+			break;
+		default:
+			return;
+	}
+});
 
 const helper = new THREE.CameraHelper(light.shadow.camera);
 scene.add(helper);
@@ -64,8 +139,8 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
-const stats = Stats()
-document.body.appendChild(stats.dom)
+const stats = Stats();
+document.body.appendChild(stats.dom);
 
 window.addEventListener('resize', onWindowResize);
 
@@ -77,6 +152,12 @@ function onWindowResize() {
 }
 
 function animate() {
+	// controls.maxDistance = 10;
+	keyListeners.forEach((listener) => listener(keysHeld));
+	console.log({ keysHeld });
+	controls.target.copy(car.position);
+	controls.update();
+	TWEEN.update();
 	requestAnimationFrame(animate);
 	render();
 	stats.update();
--- src/geometries/car.ts
+++ src/geometries/car.ts
@@ -1,30 +1,89 @@
 import * as THREE from 'three';
+import { TWEEN } from 'three/examples/jsm/libs/tween.module.min';
 
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const meshNormalMaterial = new THREE.MeshBasicMaterial({
-	wireframe: true,
+const cubeGeometry = new THREE.BoxGeometry(1, 0.6, 2);
+const meshNormalMaterial = new THREE.MeshPhongMaterial({
+	wireframe: false,
+	color: 0xffff00,
 });
 
 export const car = new THREE.Mesh(cubeGeometry, meshNormalMaterial);
 const moveCar = (key: string, mesh: THREE.Mesh, render: Function) => {
-	console.log(mesh);
+	const speed = 0.4;
+	let axis: 'z' | 'x' = 'x';
+	let direction: -1 | 1 = 1;
+	const originalPosition = mesh.position;
 	switch (key.toUpperCase()) {
 		case 'W':
-			mesh.position.y += 0.1;
+			axis = 'z';
+			direction = -1;
+			mesh.translateZ(direction * speed);
+			// mesh.position.z -= speed;w
 			break;
 		case 'S':
-			mesh.position.y -= 0.1;
+			axis = 'z';
+			direction = 1;
+			mesh.translateZ(direction * speed);
+			// mesh.position.z += speed;
 			break;
+		default:
+			return;
+	}
+
+	console.log({ direction, axis });
+
+	// new TWEEN.Tween(mesh.position)
+	// 	.to(
+	// 		{
+	// 			[axis]: originalPosition[axis] + speed * direction,
+	// 		},
+	// 		200
+	// 	)
+	// 	.easing(TWEEN.Easing.Linear.None)
+	// 	.start();
+
+	render();
+};
+
+const rotateCar = (key: string, mesh: THREE.Mesh, render: Function) => {
+	const speed = 0.4;
+	let axis: 'z' | 'x' = 'x';
+	let direction: -1 | 1 = 1;
+	const originalPosition = mesh.position;
+	switch (key.toUpperCase()) {
 		case 'A':
-			mesh.position.x -= 0.1;
+			axis = 'x';
+			direction = -1;
+			mesh.rotateY((-direction * Math.PI) / 30);
+			// mesh.position.x -= speed;
 			break;
 		case 'D':
-			mesh.position.x += 0.1;
+			axis = 'x';
+			direction = 1;
+			mesh.rotateY((-direction * Math.PI) / 30);
+			// mesh.position.x += speed;
 			break;
+
+		default:
+			return;
 	}
+
+	console.log({ direction, axis });
+
+	// new TWEEN.Tween(mesh.position)
+	// 	.to(
+	// 		{
+	// 			[axis]: originalPosition[axis] + speed * direction,
+	// 		},
+	// 		200
+	// 	)
+	// 	.easing(TWEEN.Easing.Linear.None)
+	// 	.start();
+
 	render();
 };
 
 export const attachWASDControls = (mesh: THREE.Mesh, render: Function) => {
 	document.addEventListener('keydown', (e) => moveCar(e.key, mesh, render));
+	document.addEventListener('keydown', (e) => rotateCar(e.key, mesh, render));
 };
